adcaf033644e0477754e17cd7e1acd036295afef
Emily Wessel <ettl02@gmail.com>
2018-06-15 13:46:34 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,15 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot (sound)
+  puts "#{sound}!"
+end
+
+parrot ("Squawk")
+
+def parrot (sound = "Pretty Bird!")
+  puts "#{sound}"
+end
+
+parrot ()
+
+
